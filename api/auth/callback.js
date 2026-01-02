d69c6472c40237945f356ed89445f5c75e537d67
GPO Reward Universe <marvjosa@gmail.com>
2026-01-01 23:38:50 -0300

    test callback api

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,9 +1,3 @@
 export default function handler(req, res) {
-  const { code } = req.query;
-
-  if (!code) {
-    return res.status(400).send("Missing code");
-  }
-
-  res.redirect("/verify.html");
+  res.status(200).send("API OK");
 }
